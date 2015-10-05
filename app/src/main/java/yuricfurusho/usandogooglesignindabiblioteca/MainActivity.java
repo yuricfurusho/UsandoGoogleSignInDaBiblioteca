bb0daaa0f8f33b271ff58294d9591e938574d548
yuricfurusho <yuricfurusho@livetouch.com.br>
2015-10-05 11:53:43 -0300

    Como usar nossa biblioteca do Google Sign In

--- app/src/main/java/yuricfurusho/usandogooglesignindabiblioteca/MainActivity.java
+++ app/src/main/java/yuricfurusho/usandogooglesignindabiblioteca/MainActivity.java
@@ -1,7 +1,10 @@
 package yuricfurusho.usandogooglesignindabiblioteca;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import br.livetouch.fragments.GoogleSignInFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            GoogleSignInFragment googleSignInFragment = new GoogleSignInFragment();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragContainer, googleSignInFragment, GoogleSignInFragment.class.getSimpleName())
+                    .commit();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        getSupportFragmentManager().findFragmentByTag(GoogleSignInFragment.class.getSimpleName()).onActivityResult(requestCode, resultCode, data);
     }
 }
